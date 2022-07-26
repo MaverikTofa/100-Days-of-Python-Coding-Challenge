52d3f0ddf968b3c834d2e5a1ca13111872c8f815
Mina <maveriktofa@gmail.com>
2022-07-26 19:50:03 +0400

    Day 18.3

---
+++ Day 15 - Day 52 Intermediate/Day 18 - Turtle and GUI/Day 18.3 TurtleDrawDifferentShapes.py
@@ -0,0 +1,29 @@
+from turtle import Turtle, Screen
+from random import choice
+
+
+raphael = Turtle()
+
+
+def random_pen_color():
+    color_set = ['0', '1', '2', '3', '4', '5', '6',
+                 '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+    color = "#"
+    for _ in range(6):
+        color += choice(color_set)
+    return color
+
+
+def shape(sides):
+    raphael.pencolor(random_pen_color())
+    angle = 360/sides
+    for _ in range(sides):
+        raphael.forward(100)
+        raphael.right(angle)
+
+
+for _ in range(3, 11):
+    shape(_)
+
+screen = Screen()
+screen.exitonclick()
