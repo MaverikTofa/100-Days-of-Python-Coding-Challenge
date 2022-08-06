69cddbd952092cf0327ca6aba7f616585e131fe2
Mina <maveriktofa@gmail.com>
2022-08-06 17:36:33 +0400

    snake Done

--- Day 15 - Day 52 Intermediate/Day 20+21 - Snake Game/score.py
+++ Day 15 - Day 52 Intermediate/Day 20+21 - Snake Game/score.py
@@ -7,6 +7,19 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.goto(0, 285)
         self.color('white')
         self.hideturtle()
+        self.update_score()
+
+    def update_score(self):
         self.write(f"Score: {self.score}", align='center')
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"Game Over", align='center', font=("arial,20"))
