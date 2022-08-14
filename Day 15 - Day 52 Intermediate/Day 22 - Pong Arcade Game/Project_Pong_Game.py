1ac86e0f49c473245240e12e7813526e27990ae3
Mina <maveriktofa@gmail.com>
2022-08-14 19:14:04 +0400

    Pong

--- Day 15 - Day 52 Intermediate/Day 22 - Pong Arcade Game/Project_Pong_Game.py
+++ Day 15 - Day 52 Intermediate/Day 22 - Pong Arcade Game/Project_Pong_Game.py
@@ -1,4 +1,9 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from score import Score
+import time
+
 
 screen = Screen()
 screen.bgcolor('Black')
@@ -6,30 +11,39 @@ screen.setup(800, 600)
 screen.title("PONG")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape('square')
-paddle.color('white')
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
+ball = Ball()
+score = Score()
 
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 
 screen.listen()
-screen.onkey(go_up, 'Up')
-screen.onkey(go_down, 'Down')
+
+screen.onkey(r_paddle.go_up, 'Up')
+screen.onkey(r_paddle.go_down, 'Down')
+
+screen.onkey(l_paddle.go_up, 'w')
+screen.onkey(l_paddle.go_down, 's')
 
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 330) or (ball.distance(l_paddle) < 50 and ball.xcor() < -330):
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        score.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        score.r_point()
 
 screen.exitonclick()
--- Day 15 - Day 52 Intermediate/Day 22 - Pong Arcade Game/paddle.py
+++ Day 15 - Day 52 Intermediate/Day 22 - Pong Arcade Game/paddle.py
@@ -3,11 +3,18 @@ from turtle import Turtle
 
 class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.shape('square')
         self.color('white')
-        self.width(20)
-        self.height(100)
-        self.xcor(350)
-        self.ycor(0)
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
