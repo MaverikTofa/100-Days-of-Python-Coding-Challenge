e0d46dee5000fb472bf958fcb4cc06ddf5a34411
Mina <maveriktofa@gmail.com>
2022-08-17 12:30:58 +0400

    Turtle Crossing

---
+++ Day 15 - Day 52 Intermediate/Day 23 - Turtle Crossing Game/Project_Turtle_Crossing_Game.py
@@ -0,0 +1,39 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from score import Score
+
+screen = Screen()
+screen.title("Turtle Crossing")
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+
+player = Player()
+car_manager = CarManager()
+score = Score()
+
+screen.listen()
+screen.onkey(player.go_up, 'Up')
+
+game_is_on = True
+
+
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            score.game_over()
+
+    if player.is_at_finish_line():
+        player.got_to_start()
+        car_manager.level_up()
+        score.increse_level()
+
+screen.exitonclick()
